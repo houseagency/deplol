1a0cf42ccc53c52d281ab8fabcc682e2d22dcdfb
alfred <alfred@nurd.nu>
2017-05-18 12:03:58 +0000

    can not publish without git tag

--- deplol.sh
+++ deplol.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+gitdir="$(pwd)/.git"
 
 function docker_login {
   `aws ecr get-login --region eu-west-1`
@@ -22,27 +23,49 @@ function deploy_docker_registry {
   local registry="$(docker_registry)"
   local tag="$1"
 
-  tags="$(git log --pretty=format:'%h' -n 1) $(git rev-parse HEAD) latest"
+  tags="$(git log --pretty=format:'%h' -n 1) $(git rev-parse HEAD)"
   if [ "$tag" != "" ]; then
       tags="$tags $tag"
   fi
 
+  tmpdir="$(TMPDIR="." tempfile)"
+  rm -Rf "$tmpdir"
+  git clone --depth 1 --single-branch --branch "$tag" "file://$gitdir" "$tmpdir"
+  pushd "$tmpdir"
+  git remote rm origin
   docker build . -t $(docker_registry_name)
-
   for tag in $tags; do
     docker tag $(docker_registry_name):latest "$(docker_registry):${tag}"
     docker push "$(docker_registry):${tag}"
   done
+  popd
+
+  rm -Rf "$tmpdir"
 }
 
 function publish {
   local tag="$1"
-  docker_login
+
   if [ "$tag" = "" ]; then
-    deploy_docker_registry
+    >&2 echo "Error: There must be a git tag to be published."
+    exit 6
+  fi
+
+  if ! tag_exists "$tag"; then
+    >&2 echo "Error: Git tag does not exist: $tag"
+    exit 5
+  fi
+
+  docker_login
+  deploy_docker_registry "$tag"
+}
+
+function tag_exists {
+  local tag="$1"
+  if GIT_DIR="$gitdir" git show-ref --tags | egrep -q "refs/tags/$1$"; then
+    return 0 #true - it exists
   else
-    git tag -f -a "$tag" -m "Tagged using deplol: https://github.com/houseagency/deplol"
-    deploy_docker_registry "$tag"
+    return 1 #false - it does not
   fi
 }
 
@@ -58,7 +81,7 @@ if [ "$(docker_registry)" = "" ];then
   >&2 echo "Error: No .deplol_registry file in this directory."
   exit 3
 fi
-if [ ! -e .git ]; then
+if [ ! -d "$gitdir" ]; then
   >&2 echo "Error: This directory is not a Git repository."
   exit 4
 fi
